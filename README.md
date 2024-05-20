13a5f586f3e8f938ccea1ea78255e6daaaf2647a
victor <victor.miguel@devio.com.br>
2024-05-20 19:56:25 -0300

    new - readme file

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# magaluJavascriptChallenge
-Desafio de programação na linguagem javascript que foi feito com o objetivo de alterar algumas coisas no site da Magazine Luiza
+![GitHub](https://img.shields.io/github/license/vimigueloli/magaluJavascriptChallenge?color=4e8dec) 
+![GitHub](https://img.shields.io/badge/-Javascript-4e8dec?)
+# Magalu Javascript Challenge
+
+#### Desafio de programação na linguagem javascript que foi feito com o objetivo de executar algumas alterações no site da Magazine Luiza por meio de código.
+
+## Objetivos
+Deve ser criado um código javascript para ser executado na pagina de algum produto no site da [Magazine Luiza](https://m.magazineluiza.com.br/console-sony-playstation-5-standard-edition-825gb-ps5/p/hcb4a552ed/ga/gps5/) que seja capaz de executar as seguintes alterações:
+
+1- Ao invés da mensagem de porcentagem de desconto no PIX, informar o desconto em REAL da diferença entre o preço de antes e o atual da página do produto (Lembre-se que deverá ser aplicado valores reais da página do produto, ou seja, é preciso que a diferença dos valores seja automática e não manualmente inserida).
+
+2- Esconder o botão de "Adicionar a Sacola" e alterar a cor do botão "Comprar".
+
+Durante o scroll da página, é exibida uma caixa de informações flutuante com preços e botões de compra:
+
+3- Aplicar a mesma alteração do ponto 1 nesta caixa flutuante.
+
+4- Alterar o botão "Adicionar à Sacola" pelo de "Comprar". O botão deverá ter a cor nova e também a mesma funcionalidade do botão comprar original.
+
+## Soluções e como testa-las
+
+Todas as soluções desse desafio estão nesse repositório dentro do arquivo [solution.js](). Para testar as soluções por mim desenvolvidas copie o conteúdo do arquivo [solution.js](), abra as ferramentas de desenvolvedor do google chrome, selecione a aba de "Sources", depois selecione a opção de "Snippets" no menu localizado à esquerda, adicione um novo snippet clicando em "+ New snippet", cole o conteúdo do arquivo [solution.js]() na area indicada para colocar o códico e por fim salve o snippet com o comando "CTRL+S".
+<br/>
+Ao salvar o snippet você tem uma cópia das soluções desenvolvidas por mim salvas em seu navegador e prontas para serem executadas. Agora para testar as soluções acesse a [pagina de compra de um produto da Magazine Luiza](https://m.magazineluiza.com.br/console-sony-playstation-5-standard-edition-825gb-ps5/p/hcb4a552ed/ga/gps5/), abra novamente as ferramentas de desenvolvedor do Google Chrome, se redirecione novamente para a parte das ferramentas que lista os snippets, clique no snippet criado com o botão direito do mouse e aperte a opção "Run".
---
+++ solution.js
@@ -0,0 +1,61 @@
+// funções complementares //
+const money = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+    minimumFractionDigits: 2,
+})
+function searchInArrayForm(tag){
+    let elements = document.getElementsByTagName(tag)
+    return Array.prototype.slice.call(elements)
+}
+// --------------------- //
+
+
+function changeDiscountDisplay(){
+    let array = searchInArrayForm('div')
+    array = array.filter((item)=>item.dataset.testid == 'price-default')
+    array = array.filter((item)=>Array.prototype.slice.call(item.childNodes).map((item)=>item.localName).includes('span'))
+    array.forEach((item)=>{
+        let originalValue = Number(Array.prototype.slice.call(item.childNodes)[0].innerHTML.split(';')[1].replaceAll('.','').replace(',','.'))
+        let discountedValue = Number(Array.prototype.slice.call(item.childNodes)[1].innerText.split('\n')[0].replace('R$','').replaceAll('.','').replace(',','.'))
+        Array.prototype.slice.call(item.childNodes)[2].innerHTML = `(${money.format(Number(originalValue - discountedValue).toFixed(2))} de <strong>desconto</strong>)`
+    })    
+}
+
+function changeButtons(){
+    let array = searchInArrayForm('button')
+    let bagButton = array.find((item)=>item.dataset.testid == 'bagButton')
+    let buyButton = array.find((item)=>item.dataset.testid == 'buyButton')
+    bagButton.style = 'display:none'
+    buyButton.style = 'background-color: #0086ff'
+    
+}
+
+function changeFloatPriceDisplay(){
+    let divArray = searchInArrayForm('div')
+    let floatPrice = divArray.find((item)=>item.dataset.testid=='showcase-price').childNodes[0]
+    let priceInfo = divArray.find((item)=>item.dataset.testid == 'price-default')
+    floatPrice.innerHTML = priceInfo.innerHTML
+}
+
+function changeFloatButton(){
+    let buttonArray = searchInArrayForm('button')
+    let buyButton = buttonArray.find((item)=>item.dataset.testid == 'buyButton')
+    let floatButton = buttonArray.find((item)=>item.dataset.testid == 'float-button')
+    floatButton.style = 'background-color: #0086ff'
+    floatButton.innerHTML = buyButton.innerHTML
+}
+
+
+
+// 1) Ao invés da mensagem de porcentagem de desconto no PIX, informar o desconto em REAL da diferença entre o preço de antes e o atual da página do produto (Lembre-se que deverá ser aplicado valores reais da página do produto, ou seja, é preciso que a diferença dos valores seja automática e não manualmente inserida).
+changeDiscountDisplay()
+// 2) Esconder o botão de "Adicionar a Sacola" e alterar a cor do botão "Comprar".
+changeButtons()
+
+// Durante o scroll da página, é exibida uma caixa de informações flutuante com preços e botões de compra:
+
+// 3) Aplicar a mesma alteração do ponto 1 nesta caixa flutuante.
+changeFloatPriceDisplay()
+// 4) Alterar o botão "Adicionar à Sacola" pelo de "Comprar". O botão deverá ter a cor nova e também a mesma funcionalidade do botão comprar original.
+changeFloatButton()
